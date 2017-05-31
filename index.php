82c866b5db7c27d9c7aafb75876ce4d19304ca46
Denis Bartashevich <bartashevich@ua.pt>
2017-05-31 04:07:48 +0100

    fix:  -main page

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-if(!isset($_SESSION['token']) && !($_GET["p"] == 'login' || $_GET["p"] == 'login_first' || $_GET["p"] == 'register' || $_GET["p"] == 'password_recovery')){
+if(!isset($_GET["p"])){
+    include 'inc/php/login.php';
+}
+else if(!isset($_SESSION['token']) && !($_GET["p"] == 'login' || $_GET["p"] == 'login_first' || $_GET["p"] == 'register' || $_GET["p"] == 'password_recovery')){
     header("Location: /index.php?p=login_first");
     die();
-}
-else if(!isset($_GET["p"])){
-    include 'inc/php/login.php';
 }
 else{
     include 'inc/php/'.$_GET["p"].'.php';
